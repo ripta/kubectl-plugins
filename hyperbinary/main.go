b4dd85a2319942ca237a7dc9c3eb112ec9ca12f2
Ripta Pasay <ripta@users.noreply.github.com>
2020-05-24 22:08:24 -0700

    hyperbinary: add kubectl-show to hyperbinary

--- hyperbinary/main.go
+++ hyperbinary/main.go
@@ -7,22 +7,34 @@ import (
 	"time"
 
 	"github.com/ripta/hypercmd/pkg/hypercmd"
+	"github.com/ripta/kubectl-plugins/pkg/show"
 	"github.com/ripta/kubectl-plugins/pkg/ssh"
 
 	genopts "k8s.io/cli-runtime/pkg/genericclioptions"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
+	cmd := hypercmd.NewCommand()
 	s := genopts.IOStreams{
 		In:     os.Stdin,
 		Out:    os.Stdout,
 		ErrOut: os.Stderr,
 	}
 
-	cmd := hypercmd.NewCommand()
+	k := genopts.NewConfigFlags(true)
+	k.AddFlags(cmd.Flags())
+
+	m := cmdutil.NewMatchVersionFlags(k)
+	m.AddFlags(cmd.Flags())
+
+	f := cmdutil.NewFactory(m)
+
 	cmd.AddCommand(ssh.NewCommand(s))
+	cmd.AddCommand(show.NewCommand(f, s))
 
 	if err := cmd.Resolve(os.Args[0], true).Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
