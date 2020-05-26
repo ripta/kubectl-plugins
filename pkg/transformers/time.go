769bd68c4748232bbebdf71e11707acee6eb3414
Ripta Pasay <ripta@users.noreply.github.com>
2020-05-26 00:50:41 -0700

    pkg/transformers: implement DurationToHumanDuration

--- pkg/transformers/time.go
+++ pkg/transformers/time.go
@@ -8,7 +8,27 @@ import (
 )
 
 func init() {
+	registry["DurationToHumanDuration"] = DurationToHumanDuration
 	registry["TimeToHumanDuration"] = TimeToHumanDuration
+}
+
+// DurationToHumanDuration attempts to parse the string as a timestamp.
+func DurationToHumanDuration(params map[string]string) TransformFunc {
+	return func(v interface{}) string {
+		if v == nil {
+			if def, ok := params["whenEmpty"]; ok {
+				return def
+			}
+			return ""
+		}
+		switch n := v.(type) {
+		case int64:
+			return duration.HumanDuration(time.Duration(n) * time.Second)
+		case float64:
+			return duration.HumanDuration(time.Duration(int64(n)) * time.Second)
+		}
+		return fmt.Sprintf("<invalid duration: %v (%T)>", v, v)
+	}
 }
 
 // TimeToHumanDuration attempts to parse the string as a timestamp.
